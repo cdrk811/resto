bda7afc975b4d319462f6a065290744e676a6154
Cedrick <cedrickvalencia811@gmail.com>
2022-03-01 14:56:51 +0800

    Created Resto
    description

---
+++ resources/views/customerview/home.blade.php
@@ -0,0 +1,1 @@
+@extends('customerview.partials.index')
---
+++ resources/views/customerview/partials/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-with, initial-scale=1.0">
+	<title>Resto</title>
+
+	<!-- font awesome cdn link -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+	<!-- custom css file link -->
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+	<header>
+		<a href="#" class="logo"><i class="fas fa-utensils"></i>resto.</a>
+
+		<nav class="navbar">
+			<a class="active" href="#home">Home</a>
+			<a href="#dishes">Dishes</a>
+			<a href="#about">About</a>
+			<a href="#menu">Menu</a>
+			<a href="#review">Review</a>
+			<a href="#order">Order</a>
+		</nav>
+
+		<div class="icons">
+			<i class="fas fa-bars" id="menu-bars"></i>
+			<i class="fas fa-search" id="search-icon"></i>
+			<a href="#" class="fas fa-heart"></a>
+			<a href="#" class="fas fa-shopping-cart"></a>
+		</div>
+
+
+	</header>
+
+	<!-- search form -->
+	<form action="" id="search-form">
+		<input type="search" placeholder="Search here..." name="" id="search-box">
+		<label for="search-box" class="fas fa-search"></label>
+	</form>
+
+	<!-- custom js file link -->
+	<script type="text/javascript" src="/js/script.js"></script>
+</body>
+</html>
